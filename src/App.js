5dc6278dd5eb7ecb09fad427f7452fe28b2f056c
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-12-05 23:36:34 +0530

    Desktop header completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary'
+import Navbar from './components/Navbar/NavbarIndex';
 
 function App() {
+
+  function ErrorFallBack() {
+    return <h1>Found Error. Try Again</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ErrorBoundary FallbackComponent={ErrorFallBack}>
+        <Navbar />
+      </ErrorBoundary>
+    </Router>
   );
 }
 
